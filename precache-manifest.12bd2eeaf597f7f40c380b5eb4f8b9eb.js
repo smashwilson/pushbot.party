79ab80e19b3cc5521d4ebf8cf25d5adc530969c1
Ash Wilson <smashwilson@github.com>
2019-06-21 09:10:14 -0400

    Birth a gh-pages branch

---
+++ precache-manifest.12bd2eeaf597f7f40c380b5eb4f8b9eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3d30258943045a73e93d038eb23f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6c952036abadec91a95",
+    "url": "/static/css/main.a5d92018.chunk.css"
+  },
+  {
+    "revision": "88800b27c7af23d312d3",
+    "url": "/static/js/2.c95af251.chunk.js"
+  },
+  {
+    "revision": "f6c952036abadec91a95",
+    "url": "/static/js/main.bf753140.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
