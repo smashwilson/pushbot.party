3f1d825f60b87938917d8647422a4bebdf0b7539
Ash Wilson <smashwilson@github.com>
2019-06-25 07:47:39 -0400

    Don't lose the CNAME on publish

--- actions/publish/entrypoint.sh
+++ actions/publish/entrypoint.sh
@@ -24,6 +24,7 @@ npm run build
 printf "Writing to gh-pages\n"
 
 git branch --track gh-pages refs/remotes/origin/gh-pages
+cat 'pushbot.party' > build/CNAME
 git add -f build/
 TREE=$(git write-tree --prefix=build/)
 COMMIT=$(git commit-tree "${TREE}" -p gh-pages -m "Built from ${GITHUB_SHA:-unknown sha}")
