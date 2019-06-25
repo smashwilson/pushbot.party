61d1d87e4db7e2a8d403e1fc18da6226f4119f10
Ash Wilson <smashwilson@github.com>
2019-06-25 13:48:36 -0400

    Admin-only section in site navigation

--- src/SideNav.tsx
+++ src/SideNav.tsx
@@ -1,34 +1,59 @@
-import React from "react";
+import React, {useContext} from "react";
 import {NavLink} from "react-router-dom";
+
+import {Role} from "./common/Role";
+import {PendingDiffContext, hasPendingDelta} from "./common/PendingDiff";
 
 interface PillProps {
   to: string;
   exact?: boolean;
-  children: string;
+  children: React.ReactNode;
 }
 
-const Pill = (props: PillProps) => (
-  <li role="presentation" className="nav-item">
-    <NavLink
-      to={props.to}
-      exact={props.exact}
-      className="nav-link"
-      activeClassName={"active"}
-    >
-      {props.children}
-    </NavLink>
-  </li>
-);
+function Pill(props: PillProps) {
+  return (
+    <li role="presentation" className="nav-item">
+      <NavLink
+        to={props.to}
+        exact={props.exact}
+        className="nav-link"
+        activeClassName={"active"}
+      >
+        {props.children}
+      </NavLink>
+    </li>
+  );
+}
 
-export const SideNav = () => (
-  <ul className="nav nav-pills flex-column">
-    <Pill to="/" exact>
-      dashboard
-    </Pill>
-    <Pill to="/people">dramatis personae</Pill>
-    <Pill to="/recent">recent chatter</Pill>
-    <hr />
-    <Pill to="/quotes">quotes</Pill>
-    <Pill to="/events">events</Pill>
-  </ul>
-);
+export const SideNav = () => {
+  const showBadge = hasPendingDelta(useContext(PendingDiffContext));
+
+  return (
+    <ul className="nav nav-pills flex-column">
+      <Pill to="/" exact>
+        dashboard
+      </Pill>
+      <Pill to="/people">dramatis personae</Pill>
+      <Pill to="/recent">recent chatter</Pill>
+      <hr />
+      <Pill to="/quotes">quotes</Pill>
+      <Pill to="/events">events</Pill>
+      <Role name="admin">
+        <>
+          <hr />
+          <p className="bg-warning text-white font-weight-bold p-2 rounded text-center">
+            <i className="fas fa-id-badge mr-2" />
+            admin only
+          </p>
+          <Pill to="/admin/services">services</Pill>
+          <Pill to="/admin/sync">
+            sync{" "}
+            {showBadge && (
+              <span className="badge badge-info float-right">pending</span>
+            )}
+          </Pill>
+        </>
+      </Role>
+    </ul>
+  );
+};
