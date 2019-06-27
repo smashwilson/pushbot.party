9f4eb6606e4298e6c6080ba0e8df94e4c1c605ed
Ash Wilson <smashwilson@github.com>
2019-06-27 14:08:35 -0400

    No need for StubCoordinator now

--- src/Authenticated.tsx
+++ src/Authenticated.tsx
@@ -13,137 +13,24 @@ import {Services} from "./admin/services/Services";
 import {ServiceEditor} from "./admin/services/ServiceEditor";
 import {Sync} from "./admin/sync/Sync";
 import {UserContext, IUser} from "./common/Role";
-import {CoordinatorContext, Coordinator} from "./common/coordinator";
+import {
+  CoordinatorContext,
+  Coordinator,
+  nullCoordinator,
+} from "./common/coordinator";
 import {PendingDiff} from "./common/PendingDiff";
 
 interface AuthenticatedProps {
   user: IUser;
 }
 
-class StubCoordinator extends Coordinator {
-  async getDesiredState() {
-    return {
-      units: [
-        {
-          id: 1,
-          path: "/etc/systemd/system/az-pushbot.service",
-          type: "simple" as "simple",
-          container: {
-            name: "pushbot",
-            image_name: "quay.io/smashwilson/az-pushbot",
-            image_tag: "latest",
-          },
-          secrets: ["AZ_COORDINATOR_TOKEN"],
-          env: {
-            VAR0: "zero",
-            VAR1: "one",
-          },
-          ports: {
-            "80": 8080,
-            "443": 8443,
-          },
-          volumes: {},
-        },
-      ],
-    };
-  }
-
-  async getSecrets() {
-    return ["AZ_COORDINATOR_TOKEN", "TLS_PRIVATE_KEY", "ABC_STUFF"];
-  }
-
-  async getDiff() {
-    return {
-      units_to_add: [
-        {
-          id: 1,
-          path: "/etc/systemd/system/az-added-0.service",
-          type: "simple" as "simple",
-          container: {
-            name: "az-added-0",
-            image_name: "quay.io/smashwilson/az-added-0",
-            image_tag: "latest",
-          },
-          secrets: [],
-          env: {},
-          ports: {},
-          volumes: {},
-        },
-        {
-          id: 2,
-          path: "/etc/systemd/system/az-added-1.service",
-          type: "simple" as "simple",
-          container: {
-            name: "az-added-1",
-            image_name: "quay.io/smashwilson/az-added-1",
-            image_tag: "branch",
-          },
-          secrets: [],
-          env: {},
-          ports: {},
-          volumes: {},
-        },
-      ],
-      units_to_change: [
-        {
-          id: 3,
-          path: "/etc/systemd/system/az-changed-1.service",
-          type: "simple" as "simple",
-          container: {
-            name: "az-changed-1",
-            image_name: "quay.io/smashwilson/az-changed-1",
-            image_tag: "latest",
-          },
-          secrets: [],
-          env: {},
-          ports: {},
-          volumes: {},
-        },
-      ],
-      units_to_restart: [
-        {
-          id: 3,
-          path: "/etc/systemd/system/az-restart-0.service",
-          type: "oneshot" as "oneshot",
-          container: {
-            image_name: "quay.io/smashwilson/az-restart-0",
-            image_tag: "latest",
-          },
-          secrets: [],
-          env: {},
-          ports: {},
-          volumes: {},
-        },
-      ],
-      units_to_remove: [
-        {path: "/etc/systemd/system/az-remove-0.service"},
-        {path: "/etc/systemd/system/az-remove-1.service"},
-      ],
-      files_to_write: [
-        "/etc/ssl/az/dhparams.pem",
-        "/etc/ssl/az/backend.azurefire.net/privkey.pem",
-      ],
-    };
-  }
-
-  async getSync() {
-    return {
-      in_progress: false,
-      reports: [],
-      errors: [],
-    };
-  }
-}
-
 const coordinatorForUser = memo(
   (user: IUser): Coordinator => {
-    return new StubCoordinator("");
-
-    // if (user.coordinatorToken) {
-    //   return new Coordinator(user.coordinatorToken);
-    // } else {
-    //   return nullCoordinator;
-    // }
+    if (user.coordinatorToken) {
+      return new Coordinator(user.coordinatorToken);
+    } else {
+      return nullCoordinator;
+    }
   },
   (oldArgs: [IUser], newArgs: [IUser]) => oldArgs[0].id === newArgs[0].id
 );
