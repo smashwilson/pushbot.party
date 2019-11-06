f7e8760d7d12fc699e10f9b9e6569d3fd237069a
Ash Wilson <smashwilson@github.com>
2019-11-06 10:06:58 -0500

    More type re-exports

--- src/common/userTypes.ts
+++ src/common/userTypes.ts
@@ -1,8 +1,11 @@
-import {AppQuery, AppQueryResponse} from "../__generated__/AppQuery.graphql";
+import {
+  AppQuery as OAppQuery,
+  AppQueryResponse as OAppQueryResponse,
+} from "../__generated__/AppQuery.graphql";
 import {QueryResult} from "./Transport";
 
-export type AppQuery = AppQuery;
-export type AppQueryResponse = AppQueryResponse;
+export type AppQuery = OAppQuery;
+export type AppQueryResponse = OAppQueryResponse;
 export type AppQueryResult = QueryResult<AppQuery>;
 
 export type IUser = AppQueryResponse["users"]["me"];
