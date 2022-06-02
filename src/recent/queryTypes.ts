d76b2c4a805c753f27b05aa5d3e7cb127c9a4b1e
Ash Wilson <smashwilson@github.com>
2022-06-02 11:15:54 -0400

    Change imports

--- src/recent/queryTypes.ts
+++ src/recent/queryTypes.ts
@@ -1,25 +1,17 @@
 import {QueryResult} from "../common/Transport";
 
-import {
-  RecentChannelQuery as ORecentChannelQuery,
-  RecentChannelQueryVariables as ORecentChannelQueryVariables,
-  RecentChannelQueryResponse as ORecentChannelQueryResponse,
-} from "../__generated__/RecentChannelQuery.graphql";
+import {RecentChannelQuery as ORecentChannelQuery} from "../__generated__/RecentChannelQuery.graphql";
 
-import {
-  RecentHistoryQuery as ORecentHistoryQuery,
-  RecentHistoryQueryVariables as ORecentHistoryQueryVariables,
-  RecentHistoryQueryResponse as ORecentHistoryQueryResponse,
-} from "../__generated__/RecentHistoryQuery.graphql";
+import {RecentHistoryQuery as ORecentHistoryQuery} from "../__generated__/RecentHistoryQuery.graphql";
 
 export type RecentChannelQuery = ORecentChannelQuery;
-export type RecentChannelQueryVariables = ORecentChannelQueryVariables;
-export type RecentChannelQueryResponse = ORecentChannelQueryResponse;
+export type RecentChannelQueryVariables = ORecentChannelQuery["variables"];
+export type RecentChannelQueryResponse = ORecentChannelQuery["response"];
 export type RecentChannelResult = QueryResult<RecentChannelQuery>;
 
 export type RecentHistoryQuery = ORecentHistoryQuery;
-export type RecentHistoryQueryVariables = ORecentHistoryQueryVariables;
-export type RecentHistoryQueryResponse = ORecentHistoryQueryResponse;
+export type RecentHistoryQueryVariables = ORecentHistoryQuery["variables"];
+export type RecentHistoryQueryResponse = ORecentHistoryQuery["response"];
 export type RecentHistoryResult = QueryResult<RecentHistoryQuery>;
 
 export type ILine = NonNullable<
--- src/rem/memoryTypes.ts
+++ src/rem/memoryTypes.ts
@@ -1,17 +1,13 @@
-import {
-  RandomMemoryListQuery as ORandomMemoryListQuery,
-  RandomMemoryListQueryVariables as ORandomMemoryListQueryVariables,
-  RandomMemoryListQueryResponse as ORandomMemoryListQueryResponse,
-} from "../__generated__/RandomMemoryListQuery.graphql";
+import {RandomMemoryListQuery as ORandomMemoryListQuery} from "../__generated__/RandomMemoryListQuery.graphql";
 
 import {QueryResult} from "../common/Transport";
 
 export type RandomMemoryListQuery = ORandomMemoryListQuery;
-export type RandomMemoryListQueryVariables = ORandomMemoryListQueryVariables;
-export type RandomMemoryListQueryResponse = ORandomMemoryListQueryResponse;
+export type RandomMemoryListQueryVariables =
+  ORandomMemoryListQuery["variables"];
+export type RandomMemoryListQueryResponse = ORandomMemoryListQuery["response"];
 
 export type RandomMemoryListResult = QueryResult<RandomMemoryListQuery>;
 
-import {MatchingMemoryPage_rem} from "../__generated__/MatchingMemoryPage_rem.graphql";
-
-export type MatchingMemoryPageRem = MatchingMemoryPage_rem;
+import {MatchingMemoryPage_rem$data} from "../__generated__/MatchingMemoryPage_rem.graphql";
+export type MatchingMemoryPageRem = MatchingMemoryPage_rem$data;
